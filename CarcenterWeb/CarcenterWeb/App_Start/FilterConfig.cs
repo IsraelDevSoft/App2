8aa30b9a13d9255387655507846089d96708acc4
israel1423 <santanaisrael62@gmail.com>
2019-02-28 20:47:21 -0500

    Creacion de filtro

--- CarcenterWeb/CarcenterWeb/App_Start/FilterConfig.cs
+++ CarcenterWeb/CarcenterWeb/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace CarcenterWeb
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //Habilitar el filtro de sesion para cada submit en la aplicacion
+            filters.Add(new Filters.VerificarSesion());
         }
     }
 }
---
+++ CarcenterWeb/CarcenterWeb/Filters/VerificarSesion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarcenterWeb.Models;
+using CarcenterWeb.Controllers;
+using System.Web.Mvc;
+
+namespace CarcenterWeb.Filters
+{
+    public class VerificarSesion : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //obtener de la sesion los datos de la persona logueda
+            var opersona = (PERSONAS)HttpContext.Current.Session["User"];
+            //si la sesion no exite o no se ha iniciado sesion...
+            if(opersona == null)
+            {
+                //si la peticion viene de un controlador diferente a accesocontroller, regresamos
+                //al usuario a la pantalla del login
+                if(filterContext.Controller is AccesoController == false)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Acceso/Index");
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
