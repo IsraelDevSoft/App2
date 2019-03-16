d645104126e462a8a0d3a43697e5fd948e24b77c
israel1423 <santanaisrael62@gmail.com>
2019-03-16 11:44:50 -0500

    16/03/2019 implementacion de agragar y editar

--- BD/DML_PERSONAS.sql
+++ BD/DML_PERSONAS.sql
@@ -3,3 +3,5 @@ use carcenter;
 INSERT INTO PERSONAS VALUES(
 '1234','Israel Santana', 3507745536, 'Cll 71a #77a-10', 'israelsantana373@gmail.com',
 null, 'ADM', '1234', 'A');
+
+select * from PERSONAS
--- CarcenterWeb/CarcenterWeb/Controllers/DominiosController.cs
+++ CarcenterWeb/CarcenterWeb/Controllers/DominiosController.cs
@@ -26,5 +26,69 @@ namespace CarcenterWeb.Controllers
             }
             return View(lstDominios);
         }
+
+        //metodo que llama al formulario de insercion
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        //metodo que va a guardar en la BD
+        [HttpPost]
+        public ActionResult Add(DominiosDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //insercion en la BD
+            using (carcenterEntities db = new carcenterEntities())
+            {
+                DOMINIOS oDominio = new DOMINIOS();
+                oDominio.TIPO_DOMINIO = model.TipoDominio;
+                oDominio.ID_DOMINIO = model.IdDominio;
+                oDominio.VLR_DOMINIO = model.VlrDominio;
+
+                db.DOMINIOS.Add(oDominio);
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~/Dominios/"));
+        }
+        public ActionResult Edit(String tipoDominio, String idDominio)
+        {
+            DominiosDTO model = new DominiosDTO();
+            using(carcenterEntities db = new carcenterEntities())
+            {
+                DOMINIOS oDominio = db.DOMINIOS.Find(tipoDominio, idDominio);
+                model.TipoDominio = oDominio.TIPO_DOMINIO;
+                model.IdDominio = oDominio.ID_DOMINIO;
+                model.VlrDominio = oDominio.VLR_DOMINIO;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(DominiosDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            
+            //editar registro en la BD
+            using(carcenterEntities db = new carcenterEntities())
+            {
+                DOMINIOS oDominio = db.DOMINIOS.Find(model.TipoDominio, model.IdDominio);
+                oDominio.TIPO_DOMINIO = model.TipoDominio;
+                oDominio.ID_DOMINIO = model.IdDominio;
+                oDominio.VLR_DOMINIO = model.VlrDominio;
+
+                db.Entry(oDominio).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+            return Redirect(Url.Content("~/Dominios"));
+        }
     }
 }
